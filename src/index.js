0ce2ff801ee6bedd1265bda11b8778deeec3d395
Masha <nov93523@gmail.com>
2019-10-10 22:09:26 -0400

    feat: add brackets task algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let bracketsSequenceArr = str.split("");
+    let bracketsStack = [];
+
+    for (let i = 0; i < bracketsSequenceArr.length; i++) {
+        if (checkIfOpenBracket(bracketsSequenceArr[i])) {
+            bracketsStack.push(bracketsSequenceArr[i]);
+        } else {
+            bracketsStack.pop();
+            if (bracketsStack.length === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    function checkIfOpenBracket (bracket) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j][0] === bracket) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+};
